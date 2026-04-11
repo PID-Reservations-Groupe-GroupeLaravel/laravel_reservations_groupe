1ee252c896c7547b07e0b843eb76c87d51c882e8
Salim <elg.salim@gmail.com>
2026-04-11 12:07:14 +0200

    feat(B1): add route GET /sessions (list active sessions)

--- routes/web.php
+++ routes/web.php
@@ -1,40 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\LocationController;
 use App\Http\Controllers\ShowController;
-
 use App\Http\Controllers\TypeController;
 use App\Http\Controllers\PriceController;
 use App\Http\Controllers\LocalityController;
 use App\Http\Controllers\RoleController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
+use App\Http\Controllers\SessionController;
 
 Route::view('/', 'welcome')->name('home');
-
-/*
-|--------------------------------------------------------------------------
-| Dashboard
-|--------------------------------------------------------------------------
-*/
 
 Route::view('/dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
-
-/*
-|--------------------------------------------------------------------------
-| Profile
-|--------------------------------------------------------------------------
-*/
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -42,77 +23,26 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Public routes (PDF)
-|--------------------------------------------------------------------------
-*/
+Route::middleware('auth')->group(function () {
+    Route::get('/profile/sessions', [SessionController::class, 'index'])
+        ->name('profile.sessions');
+});
 
 Route::get('/location', [LocationController::class, 'index'])->name('location.index');
-Route::get('/location/{id}', [LocationController::class, 'show'])
-    ->whereNumber('id')
-    ->name('location.show');
-
+Route::get('/location/{id}', [LocationController::class, 'show'])->whereNumber('id')->name('location.show');
 Route::get('/show', [ShowController::class, 'index'])->name('show.index');
-Route::get('/show/{id}', [ShowController::class, 'show'])
-    ->whereNumber('id')
-    ->name('show.show');
-
-/*
-|--------------------------------------------------------------------------
-| Type
-|--------------------------------------------------------------------------
-*/
-
+Route::get('/show/{id}', [ShowController::class, 'show'])->whereNumber('id')->name('show.show');
 Route::get('/type', [TypeController::class, 'index'])->name('type.index');
-Route::get('/type/{id}', [TypeController::class, 'show'])
-    ->whereNumber('id')
-    ->name('type.show');
-
-/*
-|--------------------------------------------------------------------------
-| Price
-|--------------------------------------------------------------------------
-*/
-
+Route::get('/type/{id}', [TypeController::class, 'show'])->whereNumber('id')->name('type.show');
 Route::get('/price', [PriceController::class, 'index'])->name('price.index');
-Route::get('/price/{id}', [PriceController::class, 'show'])
-    ->whereNumber('id')
-    ->name('price.show');
-
-/*
-|--------------------------------------------------------------------------
-| Locality
-|--------------------------------------------------------------------------
-*/
-
+Route::get('/price/{id}', [PriceController::class, 'show'])->whereNumber('id')->name('price.show');
 Route::get('/locality', [LocalityController::class, 'index'])->name('locality.index');
-Route::get('/locality/{postal_code}', [LocalityController::class, 'show'])
-    ->where('postal_code', '[0-9]+')
-    ->name('locality.show');
-
-/*
-|--------------------------------------------------------------------------
-| Role
-|--------------------------------------------------------------------------
-*/
-
+Route::get('/locality/{postal_code}', [LocalityController::class, 'show'])->where('postal_code', '[0-9]+')->name('locality.show');
 Route::get('/role', [RoleController::class, 'index'])->name('role.index');
-Route::get('/role/{id}', [RoleController::class, 'show'])
-    ->whereNumber('id')
-    ->name('role.show');
-
-/*
-|--------------------------------------------------------------------------
-| Admin (back-office)
-|--------------------------------------------------------------------------
-*/
+Route::get('/role/{id}', [RoleController::class, 'show'])->whereNumber('id')->name('role.show');
 
 Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin', function () {
-        return view('admin.dashboard');
-    })->name('admin.dashboard');
-
+    Route::get('/admin', function () { return view('admin.dashboard'); })->name('admin.dashboard');
     Route::resource('artists', ArtistController::class);
 });
 
