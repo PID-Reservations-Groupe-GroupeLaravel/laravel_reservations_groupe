166a91cc05525413512018d0c198c6c7bb2604be
Salim <elg.salim@gmail.com>
2026-04-14 23:58:34 +0200

    feat(B2): protect all reservation routes with auth:sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,7 @@ use App\Http\Controllers\Api\ReservationApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('reservations', ReservationApiController::class);
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', fn(Request $r) => $r->user()->load('roles'));
+    Route::apiResource('reservations', ReservationApiController::class);
 });
