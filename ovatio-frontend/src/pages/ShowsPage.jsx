e8e9df7c755f9f220ee98caca98e85e07486b42c
Soufiane Achraa <soufianeach2@gmail.com>
2026-04-22 23:32:36 +0200

    fix(shows): grid 4 columns per row

--- ovatio-frontend/src/pages/ShowsPage.jsx
+++ ovatio-frontend/src/pages/ShowsPage.jsx
@@ -62,7 +62,7 @@ export default function ShowsPage() {
             </p>
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
             {shows.map((show) => <ShowCard key={show.id} show={show} />)}
           </div>
         )}
